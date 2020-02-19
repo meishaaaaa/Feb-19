6514bb39775bb89c2cc08ded6939ced66ec14556
meishaaaaa <meishaaaaa@163.com>
2020-02-19 19:41:54 +0800

    change1

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.collection;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -8,9 +10,16 @@ public class Practice1 {
     }
 
     /**
-     *  生成10个0到20的随机整数，要求不能重复
+     * 生成10个0到20的随机整数，要求不能重复
      */
+
     public static Collection<Integer> randomNumber() {
-        return null;
+        Collection<Integer> randomNumber = new HashSet<>();
+        while (randomNumber.size() < 10) {
+            Random r = new Random();
+            int num = r.nextInt(20);
+            randomNumber.add(num);
+        }
+        return randomNumber;
     }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,19 +1,43 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
     public static void main(String[] args) {
         //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+
+        /**
+         * 创建一个List 然后在List中添加0到100整数
+         */
+        List<Integer> list = getNumbers();
+
+        //for循环
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(getNumbers().get(i));
+        }
+
+        //增强for循环
+        for (int i : list) {
+            System.out.println(i);
+        }
+
+        //迭代器
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+
     }
 
-    /**
-     * 创建一个List 然后在List中添加0到100整数
-     */
     public static List<Integer> getNumbers() {
-        return null;
+        List<Integer> getNumbers = new ArrayList<>();
+        for (int i = 0; i < 101; i++) {
+            getNumbers.add(i);
+        }
+        return getNumbers;
     }
-
-
 }
